3058069181ca36e4249e8afd439f25e23d66bf0d
Anveetha Suresh <149901570+anveetha@users.noreply.github.com>
2025-06-26 14:36:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 
-COHERE_API_KEY = '' # INPUT COHERE API KEY HERE
+COHERE_API_KEY = 'your-default-api-key-here'
 
 if not COHERE_API_KEY:
     raise ValueError("COHERE_API_KEY environment variable not set!")
